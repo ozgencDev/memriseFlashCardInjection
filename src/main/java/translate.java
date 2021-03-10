90812c7d36948cc3f8f6d95662c50c4ed1138720
AliDev <ozgencdev@gmail.com>
2021-03-10 14:18:49 +0300

    Hatalar var düzenlenecek

--- src/main/java/translate.java
+++ src/main/java/translate.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -21,8 +18,7 @@ public class translate {
         //"C:\\Users\\AliDev\\Desktop\\GitKomut.txt"
         File fl = new File(directory);
         BufferedReader reader = null;
-        ArrayList<String> foreignLang = new ArrayList<>();
-        ArrayList<String> nativeLang = new ArrayList<>();
+        ArrayList<String> foreignLang = new ArrayList<>();  ArrayList<String> nativeLang = new ArrayList<>();
         ArrayList<ArrayList<String>> flashCardHolderList = new ArrayList<>();
         try {
             reader = new BufferedReader(new FileReader(fl));
@@ -53,8 +49,9 @@ public class translate {
         }
         return flashCardHolderList;//liste
     }
-    public void memriseElements(){
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\AliDev\\Desktop\\JavaKütüphane\\chromedriver.exe");
+    public void memriseElements(String chromeDrivePath,String directory,String fileName,String fileName2,String sourceFile){
+        //chromeDrivePath = "C:\\Users\\AliDev\\Desktop\\JavaKütüphane\\chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver",chromeDrivePath);
 
         driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -69,25 +66,71 @@ public class translate {
         }
         driver.findElement(By.xpath("/html/body/div[1]/a")).click();
         driver.findElement(By.xpath("//*[@id=\"promotion-modal\"]/div/button")).click();
-        driver.findElement(By.xpath("//*[@id=\"course-5963431\"]/div/div/div[2]/div/div/div[1]/p/a")).click();//Kursu Düzenle butonu
+        driver.findElement(By.xpath("//*[@id=\"course-5963431\"]/div/div/div[2]/div/div/div[1]/p/a")).click();
         try {
             driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         }catch (Exception e){
 
         }
-        driver.findElement(By.xpath("/html/body/div[3]/div[3]/div/div[2]/a/span[2]")).click();
-        driver.findElement(By.xpath("//*[@id=\"l_13391990\"]/div[3]/table/tbody[2]/tr[2]/td[2]/input")).sendKeys("Ali");
-        driver.findElement(By.xpath("//*[@id=\"l_13391990\"]/div[3]/table/tbody[2]/tr[2]/td[3]/input")).sendKeys("Özgenç");
-        driver.findElement(By.xpath("//*[@id=\"l_13391990\"]/div[3]/table/tbody[2]/tr[2]/td[1]/i")).click();
-        driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[3]/a")).click();
+        driver.findElement(By.xpath("/html/body/div[3]/div[3]/div/div[2]/a/span[2]")).click();//Kursu Düzenle butonu
+        BufferedReader bufferedReader = null;
+        BufferedReader bufferedReaderntv = null;
+        Scanner scanner = null;
+        Scanner scannerNtv = null;
+        fileCreatorAndWriter(directory,fileName,fileName2,sourceFile);
+        try{
+            File foreignFile = new File(directory + "\\" + fileName);
+            File nativeFile = new File(directory + "\\" + fileName2);
+             bufferedReader = new BufferedReader(new FileReader(foreignFile));
+             bufferedReaderntv = new BufferedReader(new FileReader(nativeFile));
+             scanner = new Scanner(bufferedReader);
+             scannerNtv = new Scanner(bufferedReaderntv);
+
+            while(scanner != null && scannerNtv != null){
+                String addForeign = scanner.nextLine();
+                String addNative = scannerNtv.nextLine();
+                driver.findElement(By.xpath("//*[@id=\"l_13391990\"]/div[3]/table/tbody[2]/tr[2]/td[2]/input")).sendKeys(addForeign);
+                driver.findElement(By.xpath("//*[@id=\"l_13391990\"]/div[3]/table/tbody[2]/tr[2]/td[3]/input")).sendKeys(addNative);
+                driver.findElement(By.xpath("//*[@id=\"l_13391990\"]/div[3]/table/tbody[2]/tr[2]/td[1]/i")).click();
+
+            }
+        }catch (Exception e){
+            driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[3]/a")).click();
+
+        }finally {
+            try{
+                bufferedReader.close();
+                bufferedReaderntv.close();
+                scanner.close();
+                scannerNtv.close();
+            }catch (Exception e){
+
+            }
+
+
+        }
+
+
+
 
     }
-    public void fileCreatorAndWriter(String directory,String fileName,String fileName2){
+    public void fileCreatorAndWriter(String directory,String fileName,String fileName2,String sourceFile){
         File frgn = new File(directory + "\\" + fileName);
         File ntv = new File(directory + "\\" + fileName2);
+        ArrayList<ArrayList<String>> flashCardPart = findGitMethod(directory + "\\" + sourceFile);
         try{
             if(frgn.createNewFile() && ntv.createNewFile()){
+                PrintWriter writer = new PrintWriter(directory + "\\" + fileName, "UTF-8");
                 System.out.println("Yaratıldı");
+                for(String s : flashCardPart.get(0)){
+                    writer.println(s);
+                }
+                writer.close();
+                PrintWriter writer2 = new PrintWriter(directory + "\\" + fileName2, "UTF-8");
+                for(String s : flashCardPart.get(1)){
+                    writer2.println(s);
+                }
+                writer2.close();
             }else{
                 System.out.println("Yaratılmış");
             }
@@ -99,10 +142,12 @@ public class translate {
 
 
     public static void main(String[] args) {
+        String chromeDrivePath = "C:\\Users\\AliDev\\Desktop\\JavaKütüphane\\chromedriver.exe";
+        String directory = "C:\\Users\\AliDev\\Desktop\\Yeni";
         translate tr = new translate();
-        //tr.memriseElements();
+        tr.memriseElements(chromeDrivePath,directory,"foreign.txt","native.txt","GitKomut.txt");
         //tr.findGitMethod();
-        tr.fileCreatorAndWriter("C:\\Users\\AliDev\\Desktop\\Yeni","yeni2.txt","yeni3.txt");
+        //tr.fileCreatorAndWriter("C:\\Users\\AliDev\\Desktop\\Yeni","yeni2.txt","yeni3.txt","GitKomut.txt");
 
 
     }
